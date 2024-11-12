778963e3b020e7d0d345bb0d4c49efa091c16e3d
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-11-12 17:30:32 +0200

    Initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yura_kulakovskyi/Documents/C++/AP/PR10/PR10_2_A
+# Build directory: /Users/yura_kulakovskyi/Documents/C++/AP/PR10/PR10_2_A/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/yura_kulakovskyi/Documents/C++/AP/PR10/PR10_2_A/cmake-build-debug/PR10_2_tests[1]_include.cmake")
+subdirs("lib/googletest")
